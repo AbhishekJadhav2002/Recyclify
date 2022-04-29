ef4dc900ac79c23f8ba0f7d6685cb0ce8fab9197
Abhi-1712 <abhijeet171202@gmail.com>
2022-04-29 18:57:58 +0530

    search bar removed

--- src/Profile/nav.js
+++ src/Profile/nav.js
@@ -23,16 +23,7 @@ class Home extends React.Component {
           <form
             class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto"
           >
-            <div class="form-group mb-0">
-              <div class="input-group input-group-alternative">
-                <div class="input-group-prepend">
-                  <span class="input-group-text"
-                    ><i class="fas fa-search"></i
-                  ></span>
-                </div>
-                <input class="form-control" placeholder="Search" type="text" />
-              </div>
-            </div>
+           
           </form>
           {/* <!-- User --> */}
           <ul class="navbar-nav align-items-center d-none d-md-flex">
